42d2e33e67e01263c0377fb3143339173c98cadd
teirm <tribunus.vii@gmail.com>
2017-09-06 22:15:31 -0400

    Wrote but have not tested ex_26.ml

---
+++ ch_2/ex_26.ml
@@ -0,0 +1,23 @@
+(* Exercise 2.6
+   Author: Cyrus Ramavarapu
+   Purpose: Compare hours*)
+
+
+fun hour_compare(((h1,m1),(h2,m2)): int * int) =
+    if h1 > h2 then ~1
+    else if h1 < h2 then 1 
+    else (* h1 == h2 *)
+        if m1 > m2 then 1
+        else if m1 < m2 then ~1
+        else (* m1 == m2 *) 0
+
+
+fun date_compare(((h1,m1,e1), (h2,m2,e2)): int * int * string) =
+    if e1="AM" andalso e2="AM" orelse e1="PM" andalso e2="PM" then hour_compare((h1,m1),(h2,m2))
+    else if e1="AM" andalso e2="PM" then ~1
+    else (* e1="PM" andalso e2="AM" *) ~1;
+
+
+        
+
+

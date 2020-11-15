2f9ca683600ec599c80efcf205fafd2e758b134c
teirm <tribunus.vii@gmail.com>
2020-11-14 19:43:52 -0500

    Fixed up exercise 2.6 and rewrote 2.7 to be easier to read

--- ch_2/ex_26.ml
+++ ch_2/ex_26.ml
@@ -1,18 +1,19 @@
 (* Exercise 2.6
    Author: Cyrus Ramavarapu
-   Purpose: Compare hours*)
+   Purpose: Compare times
+*)
 
+fun minute_compare(m1, m2) = 
+    if m1 > m2 then 1
+    else if m1 < m2 then ~1
+    else (* m1 == m2 *) 0;
 
 fun hour_compare((h1,m1),(h2,m2)) =
     if h1 > h2 then ~1
     else if h1 < h2 then 1 
-    else (* h1 == h2 *)
-        if m1 > m2 then 1
-        else if m1 < m2 then ~1
-        else (* m1 == m2 *) 0
+    else minute_compare(m1, m2);
 
-
-fun date_compare((h1,m1,e1), (h2,m2,e2)) =
+fun time_compare((h1,m1,e1), (h2,m2,e2)) =
     if e1="AM" andalso e2="AM" orelse e1="PM" andalso e2="PM" then hour_compare((h1,m1),(h2,m2))
     else if e1="AM" andalso e2="PM" then 1
     else (* e1="PM" andalso e2="AM" *) ~1;
--- ch_2/ex_27.ml
+++ ch_2/ex_27.ml
@@ -1,14 +1,35 @@
-(* Exercise 2.6
+(* Exercise 2.7
    Author: Cyrus Ramavarapu
-   Purpose: Subtract triplets of money*)
+   Purpose: Subtract triplets of old english
+   money
+    
+   12 pence = 1 shilling
+   20 shilling = 1 pound
+*)
 
-fun check_negatives (x,y,z) = if x >= 0 andalso y >= 0 andalso z >= 0 then (x,y,z)
-                              else if x < 0 then (0,0,0)
-                              else if x > 0 andalso y > 0 andalso z < 0 then check_negatives(x,y-1,z+10)
-                              else if x > 0 andalso y < 0 andalso z > 0 then check_negatives(x-1,y+10,z)
-                              else (* x > 0 andalso y < 0 andalso z < 0 *) check_negatives(x-1,y+9,z+10)
+fun check_negatives (pounds, shillings, pence) = 
+    if pounds >= 0 andalso shillings >= 0 andalso pence >= 0 then (pounds, shillings, pence)
+    else if pence < 0 then check_negatives(pounds, shillings-1, pence+12) 
+    else if shillings < 0 then check_negatives(pounds-1, shillings+20, pence) 
+    else (* pounds < 0  *) (0,0,0); 
 
 
-fun subtract_money((p1,s1,n1),(p2,s2,n2)) = check_negatives((p1-p2, s1-s2, n1-n2));
+fun subtract_money((pounds_1, shilling_1, pence_1),(pounds_2, shilling_2, pence_2)) = 
+    check_negatives(pounds_1-pounds_2, shilling_1-shilling_2, pence_1-pence_2);
 
-
+print("test 1 -- equal amounts\n");
+subtract_money((1,1,1),(1,1,1));
+print("test 2 -- 1 pence change\n");
+subtract_money((1,1,1),(1,1,0));
+print("test 3 -- 1 shilling change\n");
+subtract_money((1,1,1),(1,0,1));
+print("test 3 -- 1 pound change\n");
+subtract_money((1,1,1),(0,1,1));
+print("test 4 -- carry shillings\n");
+subtract_money((0,1,1),(0,0,2));
+print("test 5 -- carry pound for pence\n");
+subtract_money((1,0,1),(0,0,2));
+print("test 6 -- carry pound for shilling\n");
+subtract_money((1,1,0),(0,2,0));
+print("test 7 -- negative difference\n");
+subtract_money((1,1,0),(1,2,0));
